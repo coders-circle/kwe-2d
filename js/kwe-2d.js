dbce07b0b24fe5ed9a1fbf71d8b7eac3926d5aa3
frozenhelium <fren.ankit@gmail.com>
2015-09-27 20:17:19 +0545

    Add toolbar and sidebars on both side

--- js/kwe-2d.js
+++ js/kwe-2d.js
@@ -1,10 +1,11 @@
 $(function () {
     var pstyle = 'background-color: #F5F6F7; border: 1px solid #dfdfdf; padding: 5px;';
-    var drawingContent = '<canvas id="c"  width="800" height="500"></canvas>';
+    var toolbar = '<div id="toolbar"></div>'
+    var drawingContent = '<canvas id="c"  width="800" height="250"></canvas>';
     $('#homepage').w2layout({
         name: 'layout',
         panels: [
-            { type: 'top',  size: 50, resizable: true, style: pstyle, content: 'top' },
+            { type: 'top',  size: 50, resizable: true, content: toolbar },
             { type: 'left', size: 200, resizable: true, style: pstyle, content: 'left' },
             { type: 'main', style: pstyle, content: drawingContent },
             { type: 'preview', size: '50%', resizable: true, style: pstyle, content: 'preview' },
@@ -12,6 +13,70 @@ $(function () {
             { type: 'bottom', size: 50, resizable: true, style: pstyle, content: 'bottom' }
         ]
     });
+
+    $('#toolbar').w2toolbar({
+	name: 'toolbar',
+	items: [
+		{ type: 'menu',   id: 'menu-file', caption: 'File', img: 'icon-folder', items: [
+			{ text: 'New Project', icon: 'icon-page' },
+			{ text: 'Open Project', icon: 'icon-page' },
+			{ text: 'Close Project', icon: 'icon-page' }
+		]}
+	]
+});
+
+    w2ui['layout'].content('left', $().w2sidebar({
+    	name: 'sidebar-left',
+    	img: null,
+    	nodes: [
+    		{ id: 'world-1', text: 'World-1', img: 'icon-folder', expanded: true, group: true,
+                nodes: [
+                    { id: 'world-1-entities', text: 'Entities', img: 'icon-folder',
+                        nodes: [
+                            { id: 'world-1-player', text: 'Player', img: 'icon-page' },
+                            { id: 'world-1-ground', text: 'Ground', img: 'icon-page' },
+                            { id: 'world-1-obstacle', text: 'Obstacle', img: 'icon-page' }
+                       ]}
+                ]
+    		},
+            { id: 'world-2', text: 'World-2', img: 'icon-folder', expanded: true, group: true,
+                nodes: [
+                    { id: 'world-2-entities', text: 'Entities', img: 'icon-folder',
+                        nodes: [
+                            { id: 'world-2-player', text: 'Player', img: 'icon-page' },
+                            { id: 'world-2-ground', text: 'Ground', img: 'icon-page' },
+                            { id: 'world-2-obstacle', text: 'Obstacle', img: 'icon-page' }
+                       ]}
+                ]
+    		}
+    	],
+    	onClick: function (event) {
+    		w2ui['layout'].content('preview', 'id: ' + event.target);
+    	}
+    }));
+
+
+    w2ui['layout'].content('right', $().w2sidebar({
+    	name: 'sidebar-right',
+    	img: null,
+    	nodes: [
+    		{ id: 'resources', text: 'Resources', img: 'icon-folder', expanded: true, group: true,
+                nodes: [
+                    { id: 'resource-sprites', text: 'Sprites', img: 'icon-folder',
+                        nodes: [
+                            { id: 'world-1-player', text: 'Player', img: 'icon-page' },
+                            { id: 'world-1-ground', text: 'Ground', img: 'icon-page' },
+                            { id: 'world-1-obstacle', text: 'Obstacle', img: 'icon-page' }
+                        ]
+                    }
+                ]
+    		}
+    	],
+    	onClick: function (event) {
+    		w2ui['layout'].content('preview', 'id: ' + event.target);
+    	}
+    }));
+
 });
 
 
